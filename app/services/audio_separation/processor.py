b34bce5871dbac91aa865bf38aca22c46e43ed41
eleven <yinji0208@gmail.com>
2025-02-10 14:16:01 +0800

    feat: 调整人声分离服务

--- app/services/audio_separation/processor.py
+++ app/services/audio_separation/processor.py
@@ -1,8 +1,3 @@
-import os
-import uuid
-import aiohttp
-from fastapi import HTTPException
-
 from config import settings
 from audio_separator.separator import Separator
 
@@ -11,45 +6,16 @@ class AudioSeparatorProcessor:
     def __init__(self):
         self.separator = Separator(
             output_single_stem="Vocals",
-            model_file_dir=settings.MODEL_DIR,
-            output_dir=settings.OUTPUT_DIR
+            model_file_dir=settings.MODEL_DIR
         )
 
-    async def handle_input(self, file, url, request_id):
-        """统一处理文件或 URL 逻辑"""
-        if file:
-            return await self.save_upload_file(file, request_id)
-        else:
-            return await self.download_file(url, request_id)
-
-    async def download_file(self, url: str, request_id: str) -> str:
-        """从URL下载文件"""
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                if response.status != 200:
-                    raise HTTPException(status_code=response.status, detail="Failed to download file")
-
-                filename = f"{uuid.uuid4().hex}.mp3"
-                file_path = os.path.join(settings.TEMP_DIR, filename)
-
-                with open(file_path, 'wb') as f:
-                    f.write(await response.read())
-
-                return file_path
-
-    async def save_upload_file(self, file, request_id) -> str:
-        """保存上传文件"""
-        file_path = os.path.join(settings.TEMP_DIR, f"{uuid.uuid4().hex}_{file.filename}")
-        with open(file_path, "wb") as f:
-            f.write(await file.read())
-        return file_path
-
-    def process_audio(self, input_file: str, model: str, request_id: str):
+    def process_audio(self, aduio_path: str, task_id: str, output_path):
         """处理音频"""
-        self.separator.load_model(model_filename=model)
-        # 自定义分离后的人声文件命名格式
+        self.separator.load_model(model_filename=settings.MODEL_FILENAME)
+        self.separator.output_dir = output_path
+        # 修改输出文件的命名
         output_names = {
-            "Vocals": f"vocals_output_{request_id}",
-            # "Instrumental": f"instrumental_output_{random_str}"
+            "Vocals": f"vocals_{task_id}",
+            "Instrumental": f"accompaniment_{task_id}"
         }
-        return self.separator.separate(input_file, output_names)
+        return self.separator.separate(aduio_path, output_names)
--- app/services/audio_separation/routes.py
+++ app/services/audio_separation/routes.py
@@ -2,13 +2,10 @@ import os
 import logger
 import uuid
 import time
-from fastapi import APIRouter, Form, UploadFile, File, HTTPException
-from models import ApiResponse
+from fastapi import APIRouter
+from models import SeparationResponse
 from processor import AudioSeparatorProcessor
 from config import settings
-from pydantic import ValidationError
-
-# 配置日志
 
 router = APIRouter()
 processor = AudioSeparatorProcessor()
@@ -16,11 +13,12 @@ processor = AudioSeparatorProcessor()
 new_logger = logger.CustomLogger()
 
 
-@router.post("/separate-audio/", response_model=ApiResponse)
+@router.post("/process/", response_model=SeparationResponse)
 async def separate_audio(
-        model: str = Form(...),
-        file: UploadFile = File(None),
-        url: str = Form(None),
+        audio_path: str,
+        model: str,
+        task_id: str,
+        output_path: str
 ):
     # 唯一请求ID
     request_id = uuid.uuid4().hex
@@ -29,52 +27,37 @@ async def separate_audio(
 
     try:
         # 记录请求信息
-        new_logger.info(
-            f"Request received - request_id: {request_id}, model: {model}, file: {file.filename if file else None}, url: {url}")
+        new_logger.info(f"Request received - task_id: {task_id}, model: {model}, audio_path: {audio_path}")
 
-        if not file and not url:
-            raise ValueError("Either file or url must be provided")
-        if file and url:
-            raise ValueError("Provide either file or url, not both")
+        # 处理音频文件
+        output_files = processor.process_audio(audio_path, task_id, output_path)
 
-        # 处理 url/file 保存到临时目录中
-        temp_file_path = await processor.handle_input(file, url, request_id)
-        # 处理音视频文件返回人声音频文件地址
-        output_files = processor.process_audio(temp_file_path, model, request_id)
-        new_logger.info(
-            f"remove temp file: {temp_file_path}")
-        # 删除临时文件
-        if temp_file_path and os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
-
+        # 构建响应
+        response = SeparationResponse(
+            status="success",
+            task_id=task_id,
+            separated_audio={
+                "vocals": os.path.basename(output_files[0]),
+                "accompaniment": "accompaniment.wav"  # 需要processor支持accompaniment输出
+            },
+            file_paths={
+                "vocals": output_files[0],
+                "accompaniment": output_files[1] if len(output_files) > 1 else ""
+            }
+        )
+        # 计算处理时间
         processing_time = time.time() - start_time
 
-        response = ApiResponse(
-            message="Audio separation completed successfully",
-            output_files=[f"{settings.STATIC_SERVE_URL}/{settings.OUTPUT_DIR}/{file}" for file in output_files],
-            request_id=request_id,
-            processing_time=processing_time
-        )
-        new_logger.info(f"Request completed - request_id: {request_id}, response: {response}")
+
+        new_logger.info(f"Request completed - task_id: {task_id}, response: {response}, processing_time: {processing_time}")
         return response
-
-    except ValidationError as e:
-        error_message = "Invalid input data"
-        new_logger.error(f"Request failed - request_id: {request_id}, validation_error: {e}")
-    except ValueError as e:
-        error_message = str(e)
-        new_logger.error(f"Request failed - request_id: {request_id}, error: {error_message}")
-    except HTTPException as e:
-        error_message = str(e)
-        new_logger.error(f"Request failed - request_id: {request_id}, error: {error_message}")
+    # 捕获异常
     except Exception as e:
-        error_message = str(e)
-        new_logger.error(f"Unexpected error - request_id: {request_id}, error: {e}")
+        new_logger.error(f"Request failed - task_id: {task_id}, error: {e}")
 
-    processing_time = time.time() - start_time
-    return ApiResponse(
-        message=error_message,
-        output_files=[],
-        request_id=request_id,
-        processing_time=processing_time
-    )
+        # 返回失败响应
+        return SeparationResponse(
+            status="failed",
+            task_id=task_id,
+            message=str(e)
+        )
